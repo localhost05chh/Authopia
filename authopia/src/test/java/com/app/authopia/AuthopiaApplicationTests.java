090d1888f31751507f4e7a20acf89e5d357c6a75
ss00-coder <ss00.coder@gmail.com>
2023-05-15 16:07:32 +0900

    start

---
+++ authopia/src/test/java/com/app/authopia/AuthopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.authopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthopiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
