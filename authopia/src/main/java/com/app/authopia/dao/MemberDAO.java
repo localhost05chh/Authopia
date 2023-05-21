97baed4989ce602b173151adc0593ea2c443b4d7
dev-Sub <developer.woosub@gmail.com>
2023-05-21 15:56:31 +0900

    0521 3:56 신우섭

--- authopia/src/main/java/com/app/authopia/dao/MemberDAO.java
+++ authopia/src/main/java/com/app/authopia/dao/MemberDAO.java
@@ -34,7 +34,9 @@ public class MemberDAO {
     };
 
     // 회원 조회
-
+//    public Optional<MemberVO> findByMemberId(Long Id){
+//        memberMapper
+//    }
 
     // 회원 탈퇴
     public void cancelMember(Long id){
