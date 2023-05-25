3dd60650f476047d947a351da7bf6c50fcc78e2c
JinHyeok00 <kai6879.dev@gmail.com>
2023-05-26 08:50:24 +0900

    0526 0850 손진혁

--- authopia/src/main/resources/static/js/post/detail.js
+++ authopia/src/main/resources/static/js/post/detail.js
@@ -61,6 +61,33 @@ $cancel.on('mouseout', function (event) {
     })
 })
 
+//작성시간 함수
+function elapsedTime(date) {
+    const start = new Date(date);
+    const end = new Date();
+
+    const diff = (end - start) / 1000;
+
+    const times = [
+        { name: '년', milliSeconds: 60 * 60 * 24 * 365 },
+        { name: '개월', milliSeconds: 60 * 60 * 24 * 30 },
+        { name: '일', milliSeconds: 60 * 60 * 24 },
+        { name: '시간', milliSeconds: 60 * 60 },
+        { name: '분', milliSeconds: 60 },
+    ];
+
+    for (const value of times) {
+        const betweenTime = Math.floor(diff / value.milliSeconds);
+
+        if (betweenTime > 0) {
+            return `${betweenTime}${value.name} 전`;
+        }
+    }
+    return '방금 전';
+}
+
+$(".post-register-date").next().text(elapsedTime($(".post-register-date").val()));
+
 /*댓글 뿌리기*/
 const $commentBox = $("#comment-box");
 
@@ -77,8 +104,48 @@ let commentService = (function () {
         });
     }
 
+    function remove(commentId, callback){
+        $.ajax({
+            url: `/commentes/${commentId}`,
+            type: `DELETE`,
+            success: function(){
+                if(callback){
+                    callback();
+                }
+            }
+        });
+    }
 
-    return {getList: getList};
+    function write(commentContent, callback){
+        $.ajax({
+            url: `/commentes/write`,
+            type: `post`,
+            data: JSON.stringify({memberId: memberId, postId: postId, commentContent: commentContent}),
+            contentType: "application/json;charset=utf-8",
+            success: function(){
+                if(callback){
+                    callback();
+                }
+            }
+        })
+    }
+
+    function modify(comment, callback){
+        $.ajax({
+            url: "/commentes/modify",
+            type: "put",
+            data: JSON.stringify(comment),
+            contentType: "application/json;charset=utf-8",
+            success: function(){
+                if(callback){
+                    callback();
+                }
+            }
+        })
+    }
+
+
+    return {getList: getList, remove: remove, write: write, modify: modify};
 })();
 
 commentService.getList(showList);
@@ -92,6 +159,7 @@ $(window).scroll(function(){
     }
 });
 
+/* 댓글 목록 */
 function showList(commentes) {
     let text = ``;
     commentes.forEach(comment => {
@@ -100,15 +168,71 @@ function showList(commentes) {
                         <!--글쓴이-->
                         <p class="font_title_bold_lg content_secondary">${comment.memberName}</p>
                         <!--내용-->
-                        <p class="font_label_regular_xl content_quaternary">${comment.commentContent}</p>
+                        <p class="commentContent" class="font_label_regular_xl content_quaternary">${comment.commentContent}</p>
                     </div>
+                    `
+        if(memberId == comment.memberId) {
+            text += `
+                    <span class="date">·</span>
+                    <span class="update">수정</span>
+                    <input type="text" value="${comment.commentContent}" style="display: none;">
+                    <button class="update-done" style="display: none;">수정완료</button>
+                    <span class="date">·</span>
+                    <span class="delete" data-comment-id="${comment.id}">삭제</span>
+            `
+        }
+         text +=    `
                   </li>`;
     });
-    console.log(text);
     $commentBox.append(text);
 }
+/*댓글 수정*/
+$commentBox.on("click", "span.update", function(){
+    let i = $commentBox.find("span.update").index($(this));
+    $(this).prev().css("display", "none");
+    $(this).next().next().next().css("display", "none")
+    $(".commentContent").eq(i).css("display", "none");
+    $(this).css("display", "none");
+    $(".delete").eq(i).css("display", "none");
+    $(this).next().css("display", "block");
+    $(this).next().next().css("display", "block");
+});
 
-/*게시글 뿌리기*/
+$commentBox.on("click", ".update-done", function(){
+    let i = $commentBox.find("button.cancel").index($(this));
+    let commentContent = $(this).prev().val();
+    let commentId = $(this).next().next().data("comment-id");
+    console.log(commentContent);
+    console.log(commentId);
+    let comment = new Object();
+    comment.commentContent = commentContent;
+    comment.id = commentId;
+    commentService.modify(comment, function(){
+        $commentBox.html("");
+        page = 1;
+        commentService.getList(showList);
+    });
+});
+
+/*댓글 삭제*/
+$commentBox.on("click", ".delete", function(){
+    let commentId = $(this).data("comment-id");
+    commentService.remove(commentId, function(){
+        $commentBox.html("");
+        page = 1;
+        commentService.getList(showList);
+    });
+});
+
+/* 댓글 쓰기 */
+$("#target-button").on("click", function(){
+    commentService.write($("#text-count").val(), function(){
+        $commentBox.html("");
+        page = 1;
+        commentService.getList(showList);
+        $("#text-count").val("");
+    });
+});
 
 
 
