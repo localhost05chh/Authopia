4107e51c60862c84d1a59eff16e83d6fa53a23ed
ss00-coder <ss00.coder@gmail.com>
2023-05-26 21:40:13 +0900

    0526 2140 문다은:

--- authopia/src/main/java/com/app/authopia/controller/SubscribeController.java
+++ authopia/src/main/java/com/app/authopia/controller/SubscribeController.java
@@ -1,4 +1,38 @@
 package com.app.authopia.controller;
 
+import com.app.authopia.domain.vo.SubscribeVO;
+import com.app.authopia.service.subscribe.SubscribeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/subscribe/*")
 public class SubscribeController {
+
+    private final SubscribeService subscribeService;
+    //    구독 등록
+    @PostMapping ("subscribe")
+    public RedirectView subscribeTo(SubscribeVO subscribeVO){
+        subscribeService.subscribe(subscribeVO);
+        return new RedirectView("/post/author-profile");
+    }
+    //    구독 취소
+    @GetMapping("subscribeDelete")
+    public RedirectView subscribeRevoke(Long id){
+        subscribeService.subscribeDelete(id);
+        return new RedirectView("/post/author-profile");
+    }
+
+    // 결제창으로 이동
+    @GetMapping("pay")
+    public String goToPayForm(){
+        return "post/subscribe";
+    }
 }
